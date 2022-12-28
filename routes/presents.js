8a6439a26b8e8ecc6c754f31642a4bb5e6f757fd
Gerard Guasch <haldawe@hotmail.com>
2022-12-28 13:57:41 +0100

    iteration 3 done

--- routes/presents.js
+++ routes/presents.js
@@ -3,21 +3,49 @@ const router = express.Router();
 
 const Present = require("../models/present");
 
-/* GET presents listing. */
+/* GET presents list. */
+/* ROUTE /presents */
 router.get('/', async function (req, res, next) {
   try {
     const presentsFromDB = await Present.find({});
-    res.status(200).render('presents', {presentsFromDB})
+    res.status(200).render('presents', {presentsFromDB});
   } catch (error) {
     console.error(error);
     next(error);
   }
 });
 
+/* GET new present form. */
+/* ROUTE /presents/new */
+router.get('/new', async function (req, res, next) {
+  try {
+    res.status(200).render('new');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+
+/* POST get present form. */
+/* ROUTE /presents/new */
+router.post('/new', async function (req, res, next) {
+  try {
+    const {name, image, price, recipient, description} = req.body
+    await Present.create({name, image, price, recipient, description});
+    res.status(200).redirect('/presents');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+/* GET present details. */
+/* ROUTE /presents/:id */
 router.get('/:id', async function (req, res, next) {
   try {
     const presentFromDB = await Present.findById(req.params.id);
-    res.status(200).render('present', presentFromDB)
+    res.status(200).render('present', presentFromDB);
   } catch (error) {
     console.error(error);
     next(error);
